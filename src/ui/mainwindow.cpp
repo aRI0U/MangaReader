cb903ca721e71e78af1b1d930ce94de066e940a6
Nathan Lichtlé <nathan.lichtle@gmail.com>
2020-06-06 22:21:26 +0200

    add scrollarea to always display all covers

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -1,20 +1,30 @@
 #include "mainwindow.h"
 #include "tabs/maintab/maintab.h"
 
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     setWindowTitle("MangaReader");
 
     // Create central zone
-    QTabWidget *centralTabs = new QTabWidget;
-    setCentralWidget(centralTabs);
 
     // Create main tab
     MainTab *mainTab = new MainTab;
+    mainTab->setStyleSheet("background-color:lightblue;");
 
-    // Fill central zone
-    centralTabs->addTab(mainTab, tr("Library"));
+    auto* scrollArea = new QScrollArea;
+    scrollArea->setWidget(mainTab);
+    scrollArea->setWidgetResizable(true);
+
+// tmp dont display tabs
+//    QTabWidget *centralTabs = new QTabWidget;
+//    setCentralWidget(centralTabs);
+//    centralTabs->addTab(mainTab, tr("Library"));
+
+    setCentralWidget(scrollArea); //(mainTab);
+
 }
 
 MainWindow::~MainWindow()
