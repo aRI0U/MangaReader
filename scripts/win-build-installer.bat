855a663db09dc741ff30724e4dcee3bbe8510f6d
aRI0U <alain.riou14000@yahoo.com>
2020-12-18 13:00:33 +0100

    upload new release

--- scripts/win-build-installer.bat
+++ scripts/win-build-installer.bat
@@ -1,5 +1,6 @@
 SET currentOS=windows
 SET QtIFWDir=C:\Qt\QtIFW-4.0.1\bin
+SET openSSLDir=C:\Qt\Tools\OpenSSL\Win_x64\bin
 SET configFile=config\%currentOS%.xml
 SET packagesDir=packages
 SET archiveFile=%packagesDir%\com.ariou.mangareader.root\data\MangaReader.7z
@@ -12,6 +13,8 @@ ECHO "This script will create the MangaReader installer for %currentOS%."
 PAUSE
 
 cd ..
+cp %openSSLDir%\libcrypto-1_1-x64.dll %binariesDir%
+cp %openSSLDir%\libssl-1_1-x64.dll %binariesDir%
 rm %archiveFile%
 %QtIFWDir%\archivegen.exe %archiveFile% %binariesDir%
 %QtIFWDir%\repogen.exe --update -p %packagesDir% %repoDir%
